d81ac51c15686615e856ba1dd04550427de2fd24
mustafa <1196hm@gmail.com>
2017-08-21 14:02:05 +0530

    testMessage API final

---
+++ includes/Constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mustafa Hussain
+ * Date: 16-08-2017
+ * Time: 01:52
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'test');
+
+define('USER_CREATED', 101);
+define('USER_EXIST', 102);
+define('USER_CREATION_FAILED', 103);
